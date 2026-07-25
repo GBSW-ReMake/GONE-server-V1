46cb25c4c138083427b2ac7bb1d2fdb101c9cbe1
nuza <wjdansrud092208@gmail.com>
2026-07-25 23:25:52 +0900

    fix : add missing files

---
+++ src/main/java/com/remake/gone/GoneServerV1Application.java
@@ -0,0 +1,13 @@
+package com.remake.gone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoneServerV1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoneServerV1Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/remake/gone/GoneServerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remake.gone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoneServerV1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
