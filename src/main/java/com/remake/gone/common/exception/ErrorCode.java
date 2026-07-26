f00ac6ba0780b09126ab5f7bb29119f8507fe941
nuza <wjdansrud092208@gmail.com>
2026-07-26 16:16:35 +0900

    fix: javadoc수정

--- src/main/java/com/remake/gone/common/exception/ErrorCode.java
+++ src/main/java/com/remake/gone/common/exception/ErrorCode.java
@@ -5,8 +5,7 @@ import org.springframework.http.HttpStatus;
 /**
  * 도메인별 에러 코드를 정의하는 공통 인터페이스.
  *
- * 각 도메인의 에러 코드 enum은 이 인터페이스를 구현하여
- * HTTP 상태 코드, 에러 식별 코드, 기본 메시지를 제공합니다.
+ * 각 도메인의 에러 코드 enum은 이 인터페이스를 구현하여 HTTP 상태 코드, 에러 식별 코드, 기본 메시지를 제공합니다.
  *
  * @see AuthErrorCode
  */
