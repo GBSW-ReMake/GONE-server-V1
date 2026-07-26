2c717b237c13040e1a02ffedc5f05aeeb39aaeb2
nuza <wjdansrud092208@gmail.com>
2026-07-26 15:42:44 +0900

    test-something

--- src/main/java/com/remake/gone/GoneServerV1Application.java
+++ src/main/java/com/remake/gone/GoneServerV1Application.java
@@ -3,15 +3,9 @@ package com.remake.gone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** GONE 서버 애플리케이션 진입점. */
 @SpringBootApplication
 public class GoneServerV1Application {
 
-  /**
-   * 애플리케이션을 시작합니다.
-   *
-   * @param args 커맨드라인 인수
-   */
   public static void main(String[] args) {
     SpringApplication.run(GoneServerV1Application.class, args);
   }
